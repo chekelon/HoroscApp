d64e1cf9a9535665f8109f0c15a2faf9b6981b09
Ezequiel <erodriguez@people-cloud.com>
2025-03-09 23:16:29 -0600

    initial commit

---
+++ app/src/main/java/com/ezequielrodriguez/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package com.ezequielrodriguez.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ezequielrodriguez.horoscapp.R
+import com.ezequielrodriguez.horoscapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_palmistry, container, false)
+    }
+
+
+}

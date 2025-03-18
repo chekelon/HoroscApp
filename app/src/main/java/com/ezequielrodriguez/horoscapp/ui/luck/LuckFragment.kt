1917f8ff5a13c21ff84ec5c2d9ed1c2755fee80f
Ezequiel <erodriguez@people-cloud.com>
2025-03-17 20:49:16 -0600

    animation complete

--- app/src/main/java/com/ezequielrodriguez/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/ezequielrodriguez/horoscapp/ui/luck/LuckFragment.kt
@@ -1,26 +1,119 @@
 package com.ezequielrodriguez.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import com.ezequielrodriguez.horoscapp.R
-import com.ezequielrodriguez.horoscapp.databinding.FragmentHoroscopeBinding
+import com.ezequielrodriguez.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
+
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
-    private var _binding: FragmentHoroscopeBinding? = null
+    private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulete.setOnClickListener { spinRoulete() }
+    }
+
+    private fun spinRoulete() {
+        val random = Random()
+        val degress = random.nextInt(1440) + 360
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulete,View.ROTATION,0f,degress.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
+        animator.start()
+    }
+
+    private fun slideCard(){
+        val slideUpAnimator = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+
+        slideUpAnimator.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+                binding.reverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+        })
+        binding.reverse.startAnimation(slideUpAnimator)
+    }
+
+    private fun growCard() {
+        val growAnimation = AnimationUtils.loadAnimation(requireContext(),R.anim.grow)
+
+        growAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.reverse.isVisible = false
+                showPremonitionView()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
+
+        binding.reverse.startAnimation(growAnimation)
+    }
+
+    private fun showPremonitionView() {
+        val disappearAnimation = AlphaAnimation(1.0f,0.0f)
+        disappearAnimation.duration = 200
+
+        val appearAnimation = AlphaAnimation(0.0f,1.0f)
+        appearAnimation.duration = 1000
+
+        disappearAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.preview.isVisible = false
+                binding.prediction.isVisible = true
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
+
+        binding.preview.startAnimation(disappearAnimation)
+        binding.prediction.startAnimation(appearAnimation)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_luck, container, false)
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 

bb3f259491491fc6feb350bba34e96017810fe0d
Ezequiel <erodriguez@people-cloud.com>
2025-03-17 23:08:49 -0600

    add dependecie cameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,13 @@ dependencies {
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
 
+    //CameraX
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
+    implementation (libs.androidx.camera.extensions)
+
 
 
     implementation(libs.androidx.core.ktx)

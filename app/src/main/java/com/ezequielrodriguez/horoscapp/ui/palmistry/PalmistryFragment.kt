02fc6cebc2739f412281bc236c5a1980739017b0
Ezequiel <erodriguez@people-cloud.com>
2025-03-19 10:52:49 -0600

    permission management

--- app/src/main/java/com/ezequielrodriguez/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/ezequielrodriguez/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -5,21 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ezequielrodriguez.horoscapp.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import com.ezequielrodriguez.horoscapp.databinding.FragmentPalmistryBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class PalmistryFragment : Fragment() {
+
+    companion object{
+        private const val CAMERA_PERMISSION = "android.Manifest.permission.CAMERA"
+    }
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher=registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ isGranted->
+        if(isGranted){
+            //startCamera
+        }else{
+            Toast.makeText(requireContext(),"Aceptar los permisos para poder disfrutar de una experiencia mágica",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(checkCameraPermission()){
+            //Tiene permisos aceptados
+            //startCamera
+        }else{
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+    }
+
+    fun  checkCameraPermission():Boolean{
+        return PermissionChecker.checkSelfPermission(requireContext(),CAMERA_PERMISSION) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_palmistry, container, false)
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
 
